a7b4a3404719264bf6b20c51b2bbb68b0aed2a98
junghyeonsu <jung660317@naver.com>
2023-10-27 15:35:35 +0900

    chore: 데모 준비

---
+++ icona.config.js
@@ -0,0 +1,28 @@
+import { generate } from "@icona/generator";
+
+generate({
+  config: {
+    svg: {
+      active: true, // you can disable svg generator if you set false
+      path: "svg", // will generate svg files in svg folder
+      svgoConfig: {},
+    },
+    drawable: {
+      active: true, // you can disable drawable generator if you set false
+      path: "drawable", // will generate xml files in drawable folder
+      svg2vectordrawableConfig: {},
+      defaultColor: "#000000", // default color for android vector drawable
+    },
+    pdf: {
+      active: true, // you can disable pdf generator if you set false
+      path: "pdf", // will generate pdf files in pdf folder
+      pdfKitConfig: {},
+      svgToPdfOptions: {},
+    },
+    react: {
+      active: true, // you can disable react generator if you set false
+      path: "react", // will generate react component files in react folder
+      svgrConfig: {},
+    },
+  },
+});
