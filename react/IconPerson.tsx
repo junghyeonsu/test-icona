d5f73b95999e03c92a239f88841c28406ca46e3d
junghyeonsu <jung660317@naver.com>
2023-10-28 08:16:37 +0000

    [Icona] Generate Files

---
+++ react/IconPerson.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+const IconPerson = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={18}
+    height={20}
+    fill="none"
+    {...props}
+  >
+    <g fill="#212124" fillRule="evenodd" clipRule="evenodd">
+      <path d="M8.6 0a5.6 5.6 0 1 0 0 11.2A5.6 5.6 0 0 0 8.6 0ZM4.2 5.6a4.4 4.4 0 1 1 8.8 0 4.4 4.4 0 0 1-8.8 0ZM8.6 12C3.409 12 0 14.83 0 18.6a.6.6 0 0 0 .6.6h16a.6.6 0 0 0 .6-.6c0-3.77-3.409-6.6-8.6-6.6Zm0 1.2c4.47 0 7.023 2.195 7.361 4.8H1.24c.338-2.605 2.892-4.8 7.361-4.8Z" />
+    </g>
+  </svg>
+);
+export default IconPerson;
