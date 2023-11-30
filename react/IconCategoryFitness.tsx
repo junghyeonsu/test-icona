7a5a4fb330be80eb29ffbd8e919af7e5a33c69a8
junghyeonsu <jung660317@naver.com>
2023-11-30 06:11:18 +0000

    [Icona] Generate Files

---
+++ react/IconCategoryFitness.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+const IconCategoryFitness = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#212124"
+      d="M21.9 7h-1.18V5.5a2.5 2.5 0 0 0-5 0V10H9.18V5.5a2.5 2.5 0 0 0-5 0V7H3c-1.1 0-2 .9-2 2v5c0 1.1.9 2 2 2h1.18v1.5a2.5 2.5 0 0 0 5 0V13h6.54v4.5a2.5 2.5 0 0 0 5 0V16h1.18c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2Z"
+    />
+  </svg>
+);
+export default IconCategoryFitness;
