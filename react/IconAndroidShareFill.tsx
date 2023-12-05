045bae33d0cbc7c0fcd91e3bea3b78a91f2b0d21
junghyeonsu <jung660317@naver.com>
2023-12-05 09:05:51 +0000

    [Icona] Generate Files

---
+++ react/IconAndroidShareFill.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+const IconAndroidShareFill = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#212124"
+      fillRule="evenodd"
+      d="M15.2 5.5a3.3 3.3 0 1 1 .952 2.318l-7.388 3.694a3.327 3.327 0 0 1 0 .976l7.388 3.694a3.3 3.3 0 1 1-.816 1.38L8.148 13.97a3.3 3.3 0 1 1 0-3.938l7.186-3.593A3.3 3.3 0 0 1 15.2 5.5Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default IconAndroidShareFill;
---
+++ react/IconPhotoEditRotateFill.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+const IconPhotoEditRotateFill = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <g fill="#212124">
+      <path d="M11.85 4.85c-.88 0-1.72.162-2.494.457a1 1 0 1 1-.712-1.869 9 9 0 0 1 11.276 12.4l.414-.137a1 1 0 0 1 .632 1.898l-3 1a1 1 0 0 1-1.303-.785l-.5-3a1 1 0 0 1 1.973-.329l.056.333A7 7 0 0 0 11.85 4.85ZM5.614 9.165l-.063-.373a7 7 0 0 0 8.794 9.6 1 1 0 0 1 .714 1.869A9 9 0 0 1 3.801 7.82l-.385.128a1 1 0 1 1-.632-1.898l3-1a1 1 0 0 1 1.302.785l.5 3a1 1 0 1 1-1.973.329Z" />
+    </g>
+  </svg>
+);
+export default IconPhotoEditRotateFill;
